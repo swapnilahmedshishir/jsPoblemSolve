98fc96ed22ca6846147f76b68657f712fe37742d
@swapnilahmedshishir <ahmed66882298@gmail.com>
2022-05-22 12:38:30 +0600

    item Modify

--- assinment/listItem.js
+++ assinment/listItem.js
@@ -1,7 +1,17 @@
 document.getElementById('btn').addEventListener('click',function(){
     let parentItem = document.getElementById('mylist');
     let childItem = document.createElement('li');
-     childItem.innerHTML = document.getElementById('item_add').value;
+     let itemVlaue =  document.getElementById('item_add').value;
+     
+     
+     if(itemVlaue == ""){
+       let noData ="No item";
+       childItem.innerHTML = noData;
+     }else{
+        childItem.innerHTML = itemVlaue;
+        
+     }
+    
      parentItem.appendChild(childItem);
     
 });
